791f8d8032348e0819306d58f9eb468ee9b154b3
Adrien Champion <AdrienChampion@users.noreply.github.com>
2021-11-22 18:06:01 +0100

    writer module: doc

--- src/writer.rs
+++ src/writer.rs
@@ -11,40 +11,88 @@
 * limitations under the License.
 */
 
-use crate::OperationError;
 use ton_types::{BuilderData, SliceData};
 
-use crate::debug::{DbgNode, DbgPos};
+use crate::{
+    debug::{DbgNode, DbgPos},
+    OperationError,
+};
 
-pub trait Writer : 'static {
+/// Writes the result of compiling some code.
+pub trait Writer: 'static {
+    /// Constructor.
     fn new() -> Self;
+    /// Writes a command with no additional data.
     fn write_command(&mut self, command: &[u8], dbg: DbgNode) -> Result<(), OperationError>;
-    fn write_command_bitstring(&mut self, command: &[u8], bits: usize, dbg: DbgNode) -> Result<(), OperationError>;
-    fn write_composite_command(&mut self, code: &[u8], reference: BuilderData, pos: DbgPos, dbg: DbgNode) -> Result<(), OperationError>;
+    /// Writes a command and some bits to the data (bitstring) of a cell.
+    fn write_command_bitstring(
+        &mut self,
+        command: &[u8],
+        bits: usize,
+        dbg: DbgNode,
+    ) -> Result<(), OperationError>;
+    /// Writes a composite command.
+    fn write_composite_command(
+        &mut self,
+        code: &[u8],
+        reference: BuilderData,
+        pos: DbgPos,
+        dbg: DbgNode,
+    ) -> Result<(), OperationError>;
+    /// Finalizes the writing process.
     fn finalize(self) -> (BuilderData, DbgNode);
 }
 
+/// First TON codepage.
+///
+/// A codepage gives semantics to opcodes. More codepages may be added in the future and modify
+/// existing opcodes (semantics or encoding) or add new ones.
+///
+/// # Invariants
+///
+/// - `self.cells.len() == self.dbg.len()` (should just have one vector);
+/// - all cells in [`self.cells`] have at least one free cell-ref (needed for [`Self::finalize`]).
 pub(crate) struct CodePage0 {
+    /// List of cells being constructed.
     cells: Vec<BuilderData>,
+    /// Debug information (source-position and such) of cells.
+    ///
+    /// Must have the same length as [`self.cells`].
     dbg: Vec<DbgNode>,
 }
 
 impl Writer for CodePage0 {
-    /// Constructs new Writer
     fn new() -> Self {
         Self {
             cells: vec![BuilderData::new()],
             dbg: vec![DbgNode::new()],
         }
     }
-    /// writes simple command
+
     fn write_command(&mut self, command: &[u8], dbg: DbgNode) -> Result<(), OperationError> {
         self.write_command_bitstring(command, command.len() * 8, dbg)
     }
-    fn write_command_bitstring(&mut self, command: &[u8], bits: usize, dbg: DbgNode) -> Result<(), OperationError> {
+
+    /// Writes a command to a cell's data.
+    ///
+    /// - to the last cell in the list of cell if any,
+    /// - to a new cell otherwise.
+    fn write_command_bitstring(
+        &mut self,
+        command: &[u8],
+        bits: usize,
+        dbg: DbgNode,
+    ) -> Result<(), OperationError> {
+        // #optim rewrite to an `if let Some(_)`
         if !self.cells.is_empty() {
             let offset = self.cells.last().unwrap().bits_used();
-            if self.cells.last_mut().unwrap().append_raw(command, bits).is_ok() {
+            if self
+                .cells
+                .last_mut()
+                .unwrap()
+                .append_raw(command, bits)
+                .is_ok()
+            {
                 self.dbg.last_mut().unwrap().inline_node(offset, dbg);
                 return Ok(());
             }
@@ -57,21 +105,26 @@ impl Writer for CodePage0 {
         }
         Err(OperationError::NotFitInSlice)
     }
-    /// writes command with additional reference
+
+    /// Writes a composit command.
+    ///
+    /// - to the last cell in the list of cell if any,
+    /// - to a new cell otherwise.
     fn write_composite_command(
-        &mut self, 
-        command: &[u8], 
+        &mut self,
+        command: &[u8],
         reference: BuilderData,
-        pos: DbgPos, 
+        pos: DbgPos,
         dbg: DbgNode,
     ) -> Result<(), OperationError> {
+        // #optim rewrite to an `if let Some(_)`
         if !self.cells.is_empty() {
             let mut last = self.cells.last().unwrap().clone();
             let offset = last.bits_used();
             if last.references_free() > 1 // one cell remains reserved for finalization
                 && last.append_raw(command, command.len() * 8).is_ok()
-                && last.checked_append_reference(reference.clone().into_cell().map_err(|_| OperationError::NotFitInSlice)?).is_ok() {
-
+                && last.checked_append_reference(reference.clone().into_cell().map_err(|_| OperationError::NotFitInSlice)?).is_ok()
+            {
                 *self.cells.last_mut().unwrap() = last;
 
                 let node = self.dbg.last_mut().unwrap();
@@ -80,10 +133,14 @@ impl Writer for CodePage0 {
                 return Ok(());
             }
         }
+
         let mut code = BuilderData::new();
-        let cell = reference.into_cell().map_err(|_| OperationError::NotFitInSlice)?;
+        let cell = reference
+            .into_cell()
+            .map_err(|_| OperationError::NotFitInSlice)?;
         if code.append_raw(command, command.len() * 8).is_ok()
-            && code.checked_append_reference(cell).is_ok() {
+            && code.checked_append_reference(cell).is_ok()
+        {
             self.cells.push(code);
 
             let mut node = DbgNode::new();
@@ -95,23 +152,47 @@ impl Writer for CodePage0 {
         }
         Err(OperationError::NotFitInSlice)
     }
-    /// puts every cell as a reference to the previous one
+
+    /// Finalizes the writer.
+    ///
+    /// Say the list of cells in `self` is `c_0, c_1, ..., c_n`. Then this function adds `c_i` as a
+    /// *"reference"* of `c_i-1` for `i > 1`, and yields `c_0`. It might not be added as an actual
+    /// reference though, if possible `c_i` will be inlined in `c_i-1`; *possible* here means that
+    /// `c_i-1` has enough free cell-refs to add `c_i`'s cell refs.
     fn finalize(mut self) -> (BuilderData, DbgNode) {
         let mut cursor = self.cells.pop().expect("cells can't be empty");
         let mut dbg = self.dbg.pop().expect("dbgs can't be empty");
+        // #optim rewrite as `while let Some(_)`
         while !self.cells.is_empty() {
-            let mut destination = self.cells.pop()
-                .expect("vector is not empty");
+            let mut destination = self.cells.pop().expect("vector is not empty");
             let offset = destination.bits_used();
-            let slice = SliceData::from(cursor.clone().into_cell().expect("failure while convert BuilderData to cell"));
+            let slice = SliceData::from(
+                cursor
+                    .clone()
+                    .into_cell()
+                    .expect("failure while convert BuilderData to cell"),
+            );
             let mut next = self.dbg.pop().expect("dbg vector is not empty");
-            // try to inline cursor into destination
+
+            // Try to inline `cursor` into `destination`.
+            //
+            // Only possible if the `destination` has enough free ref-cells to store `cursor`'s
+            // ref-cells.
             if destination.references_free() >= cursor.references_used()
-                && destination.checked_append_references_and_data(&slice).is_ok() {
+                && destination
+                    .checked_append_references_and_data(&slice)
+                    .is_ok()
+            {
                 next.inline_node(offset, dbg);
-            // otherwise just attach cursor to destination as a reference
             } else {
-                destination.append_reference_cell(cursor.into_cell().expect("failure while convert BuilderData to cell"));
+                // Otherwise just add `cursor` to `destination` as a reference.
+                //
+                // Only legal if `cursor` has at least one free ref-cell.
+                destination.append_reference_cell(
+                    cursor
+                        .into_cell()
+                        .expect("failure while converting `BuilderData` to `Cell`"),
+                );
                 next.append_node(dbg);
             }
             cursor = destination;
